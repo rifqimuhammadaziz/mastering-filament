681bc8c4fef9715ffbd01ae1390ae2236876611e
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2024-07-29 08:51:54 +0700

    create resource modifers & resource filters

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -13,6 +13,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class ProductResource extends Resource
 {
@@ -30,36 +31,75 @@ class ProductResource extends Resource
                     ->schema([
                         Forms\Components\Section::make()
                             ->schema([
-                                Forms\Components\TextInput::make('name'),
-                                Forms\Components\TextInput::make('slug'),
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->live(onBlur: true)
+                                    ->unique()
+                                    ->afterStateUpdated(function (string $operation, $state, Forms\Set $set) {
+                                        if ($operation !== 'create') {
+                                            return;
+                                        }
+                                        $set('slug', Str::slug($state)); // set slug as product name after updated
+                                    }),
+
+                                Forms\Components\TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required()
+                                    ->unique(Product::class, 'slug', ignoreRecord: true),
                                 Forms\Components\MarkdownEditor::make('description')
                                     ->columnSpanFull(),
+
                             ])->columns(2),
                         Forms\Components\Section::make('Pricing & Inventory')
                             ->schema([
-                                Forms\Components\TextInput::make('sku'),
-                                Forms\Components\TextInput::make('price'),
-                                Forms\Components\TextInput::make('quantity'),
+                                Forms\Components\TextInput::make('sku')
+                                    ->label("SKU (Stock Keeping Unit)")
+                                    ->unique()
+                                    ->required(),
+
+                                Forms\Components\TextInput::make('price')
+                                    ->numeric()
+                                    ->rules(['regex:/^\d{1,6}(\.\d{0,2})?$/'])
+                                    ->required(),
+
+                                Forms\Components\TextInput::make('quantity')
+                                    ->numeric()
+                                    ->minValue(0)
+                                    ->maxValue(1000)
+                                    ->required(),
                                 Forms\Components\Select::make('type')
                                     ->options([
                                         'downloadable' => ProductType::DOWNLOADABLE->value,
                                         'deliverable' => ProductType::DELIVERABLE->value,
-                                    ]),
+                                    ])->required(),
                             ])->columns(2)
                     ]),
                 Forms\Components\Group::make()
                     ->schema([
                         Forms\Components\Section::make('Status')
                             ->schema([
-                                Forms\Components\Toggle::make('is_visible'),
-                                Forms\Components\Toggle::make('is_featured'),
+                                Forms\Components\Toggle::make('is_visible')
+                                    ->label('Visibility')
+                                    ->helperText('Enable or Disable product visibility')
+                                    ->default(true),
+
+                                Forms\Components\Toggle::make('is_featured')
+                                    ->label('Featured')
+                                    ->helperText('Enable or Disable product featured status'),
+
                                 Forms\Components\DatePicker::make('published_at')
+                                    ->label('Availability')
                                     ->default(now()),
                             ]),
 
                         Forms\Components\Section::make('Image')
                             ->schema([
-                                Forms\Components\FileUpload::make('image'),
+                                Forms\Components\FileUpload::make('image')
+                                    ->directory('form-attachment')
+                                    ->preserveFilenames()
+                                    ->image()
+                                    ->imageEditor(),
                             ])->collapsible(),
 
                         Forms\Components\Section::make('Associations')
@@ -76,16 +116,47 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('brand.name'),
-                Tables\Columns\IconColumn::make('is_visible')->boolean(),
-                Tables\Columns\TextColumn::make('price'),
-                Tables\Columns\TextColumn::make('quantity'),
-                Tables\Columns\TextColumn::make('published_at'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\IconColumn::make('is_visible')
+                    ->sortable()
+                    ->toggleable()
+                    ->label('Visibility')
+                    ->boolean(),
+
+                Tables\Columns\TextColumn::make('price')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('quantity')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('published_at')
+                    ->date()
+                    ->sortable(),
+
                 Tables\Columns\TextColumn::make('type'),
             ])
             ->filters([
-                //
+                Tables\Filters\TernaryFilter::make('is_visible')
+                    ->label('Visibility')
+                    ->boolean()
+                    ->trueLabel('Only Visible Products')
+                    ->falseLabel('Only Hidden Products')
+                    ->native(false),
+
+                Tables\Filters\SelectFilter::make('brand')
+                    ->relationship('brand', 'name')
+
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
